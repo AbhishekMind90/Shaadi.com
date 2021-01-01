2e75eb8e92d4b3a614620c412a823c5f101e7045
AbhishekMind90 <abhishekmind90@gmail.com>
2021-01-01 21:57:46 +0530

    Added priorities

--- Shaadi.com/src/test/java/com/shaadi/testcases/Login.java
+++ Shaadi.com/src/test/java/com/shaadi/testcases/Login.java
@@ -1,12 +1,12 @@
-package com.shaadi.login;
+package com.shaadi.testcases;
 
 import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumDriver;
@@ -17,7 +17,7 @@ import io.appium.java_client.remote.MobileCapabilityType;
 public class Login {
 
 	public static AppiumDriver<MobileElement> driver;		
-	@BeforeMethod
+	@BeforeClass
 	public static void shaadiInstall() throws Exception {			
 		DesiredCapabilities cap = new DesiredCapabilities();		
 		cap.setJavascriptEnabled(true);
@@ -25,28 +25,28 @@ public class Login {
 		cap.setCapability(MobileCapabilityType.UDID,"emulator-5554");
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"9.0.0");
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME,"AppiumAndroidDevice");	       
-		cap.setCapability(MobileCapabilityType.APP,"Add apk path");
+		cap.setCapability(MobileCapabilityType.APP,"C:\\Shaadi.com\\shaadi.apk");
 		cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,"120");
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-		cap.setCapability("appActivity","");
+		cap.setCapability("appActivity","com.shaadi.android.ui.splash.SplashScreenActivity");
 		cap.setCapability("appPackage","com.shaadi.android");       
 		URL url =new URL("http://127.0.0.1:4723/wd/hub");
 		driver = new AppiumDriver<MobileElement>(url,cap);	        
 	}
-	@Test
-	public static void loginShaadiA() throws InterruptedException{	
+	@Test(enabled=true, priority=1)
+	public void loginShaadiA(){	
 		driver.findElement(By.id("com.shaadi.android:id/btn_morph_login")).click();
 		driver.findElement(By.id("com.shaadi.android:id/edt_username")).sendKeys("abc@gmail.com");
 		driver.findElement(By.id("com.shaadi.android:id/edt_password")).sendKeys("1234567");
 		driver.findElement(By.id("com.shaadi.android:id/btn_login")).click();
 	}
-	@Test
+	@Test(enabled=true, priority=2)
 	public void navigatingToMyShaadiB() {
 		driver.findElement(By.id("com.shaadi.android:id/menu_skip")).click(); //skipping update to premium ad
 		driver.findElement(By.id("com.shaadi.android:id/rlMyShaadi")).click();
 	}
-	@Test
-	public void varifyingWidgetC() {	
+	@Test(enabled=true, priority=3)
+	public void varifyingWidgetC() {
 		MobileElement premiumMatches = driver.findElement(By.id("com.shaadi.android:id/tv_title"));
 		boolean premiumM = premiumMatches.isDisplayed();
 		Assert.assertEquals(premiumM, true);
@@ -58,6 +58,7 @@ public class Login {
 		boolean newM = newMatches.isDisplayed();
 		Assert.assertEquals(newM, true);
 	}
+	@Test(enabled=true, priority=4)
 	public void sendingConnectD() {
 		driver.findElement(By.id("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.LinearLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout")).click();
 
@@ -67,8 +68,8 @@ public class Login {
 		newMatches.click();
 		driver.findElement(By.id("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.LinearLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[2]/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout")).click();
 	}
-	@AfterMethod 
+	@AfterClass
 	public static void teardown() {
-		/*driver.removeApp(""); */
+		// driver.removeApp("");
 	} 
 }
